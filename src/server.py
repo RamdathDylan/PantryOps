251a182978ed659a56e2a573c6a438f0cd76ec11
Dylan Ramdath <ramdathdylan@gmail.com>
2026-03-04 16:47:14 -0500

    Added health endpoint and helloworld endpoint for testing purposes.

---
+++ src/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from api.health import Health
+from api.helloWorld import HelloWorld
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(Health, "/")
+    api.add_resource(HelloWorld, "/hello")
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host="127.0.0.1", port=4999, debug=True)    
