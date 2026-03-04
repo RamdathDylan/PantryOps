31b08deeb409e415d952ac5ab929f4c40b743a69
Dylan Ramdath <ramdathdylan@gmail.com>
2026-03-04 16:57:01 -0500

    Added return statment for creat_app() function.

--- src/server.py
+++ src/server.py
@@ -10,6 +10,8 @@ def create_app():
     api.add_resource(Health, "/")
     api.add_resource(HelloWorld, "/hello")
 
+    return app
+
 if __name__ == '__main__':
     app = create_app()
     app.run(host="127.0.0.1", port=4999, debug=True)    
