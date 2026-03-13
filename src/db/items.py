87886a45d44e3a6e8f133d64c2a5813f6aa635a6
Dylan Ramdath <ramdathdylan@gmail.com>
2026-03-12 22:52:45 -0400

    Adding initial get db calls for items.

---
+++ src/db/items.py
@@ -0,0 +1,42 @@
+from src.db.db_utils import *
+
+def get_all_items():
+    """
+    Queries the database and returns every record 
+    in the items table with their information. 
+
+    Ordered by id number. 
+
+    Returns:
+    List of tuples.
+    """
+
+    sql = """
+    SELECT id, name, category, unit, description 
+    FROM items
+    ORDER BY id
+    """
+
+    return exec_get_all(sql)
+
+def get_item(item_id):
+    """
+    Queries the database and returns the record with the matching 
+    id number in the items table.
+
+    Keyword Arguments:
+    id - The id of the item that is being queried.
+
+    Return:
+    Tuple or none if record not found.
+    """
+
+    sql = """
+    SELECT id, name, category, unit, description
+    FROM items
+    WHERE id=%s
+    """
+
+    return exec_get_one(sql, (item_id,))
+
+
